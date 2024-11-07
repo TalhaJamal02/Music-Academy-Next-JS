05f11ae91ec44a2761c04ca94c8520a21c878a2f
Talha Jamal <chrisLandson057@gmail.com>
2024-11-07 16:39:54 +0500

    Update page.tsx

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -2,50 +2,36 @@
 
 import React from "react";
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
-import courseData from "@/data/music-courses.json"
+import courseData from "@/data/music-courses.json";
 
 interface Course {
-  title: string,
-  description: string,
+  id: string;
+  title: string;
+  description: string;
 }
-
 
 function AllCourses() {
   return (
     <div className="min-h-screen bg-black py-12 pt-36">
-      <h1 className="text-lg md:text-5xl text-center font-sans font-bold mb-8 text-white">All courses ({courseData.courses.length})</h1>
+      <h1 className="text-lg md:text-5xl text-center font-sans font-bold mb-8 text-white">
+        All courses ({courseData.courses.length})
+      </h1>
       <div className="flex flex-wrap justify-center">
         {courseData.courses.map((course: Course) => (
-         key={courses.id}
-          <CardContainer className="inter-var m-4">
-            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
-              <CardItem
-                translateZ="50"
-                className="text-xl font-bold text-neutral-600 dark:text-white"
-              >
+          <CardContainer key={course.id} className="inter-var m-4">
+            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
+              <CardItem translateZ="50" className="text-xl font-bold text-neutral-600 dark:text-white">
                 {course.title}
               </CardItem>
-              <CardItem
-                as="p"
-                translateZ="60"
-                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-              >
+              <CardItem as="p" translateZ="60" className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300">
                 {course.description}
               </CardItem>
 
               <div className="flex justify-between items-center mt-20">
-                <CardItem
-                  translateZ={20}
-                  as="button"
-                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-                >
+                <CardItem translateZ={20} as="button" className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white">
                   Try now →
                 </CardItem>
-                <CardItem
-                  translateZ={20}
-                  as="button"
-                  className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-                >
+                <CardItem translateZ={20} as="button" className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold">
                   Sign up
                 </CardItem>
               </div>
@@ -54,7 +40,7 @@ function AllCourses() {
         ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default AllCourses
+export default AllCourses;
